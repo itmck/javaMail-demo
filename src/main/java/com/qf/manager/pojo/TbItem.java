d9510a46c0106f604149193234599c6d997aed4f
itmck <347451331@qq.com>
2018-10-17 15:34:21 +0800

    拦截器

---
+++ src/main/java/com/qf/manager/pojo/TbItem.java
@@ -0,0 +1,38 @@
+package com.qf.manager.pojo;
+
+/**
+ * Create by it_mck 2018/10/17 13:36
+ *
+ * @Description:
+ * @Version: 1.0
+ */
+public class TbItem {
+
+    private String name;
+    private Double money;
+    private String describe;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+}
